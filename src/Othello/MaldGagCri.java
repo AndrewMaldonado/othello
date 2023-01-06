4b1db44043a3d74927687003269167f0547f947b
Nathaniel Crivelli <nathaniel.crivelli@mrjaffesclass.com>
2023-01-06 13:13:27 -0800

    SudoCode

--- src/Othello/MaldGagCri.java
+++ src/Othello/MaldGagCri.java
@@ -4,7 +4,6 @@
  * All rights reserved
  */
 
- 
 
 import java.util.ArrayList;
 
@@ -12,97 +11,111 @@ import java.util.ArrayList;
  * Test player
  */
 public class MaldGagCri extends Player {
-  // i am cool
-  /**
-   * Constructor
-   * @param name Player's name
-   * @param color Player color: one of Constants.BLACK or Constants.WHITE
-   */
-  public MaldGagCri(int color) {
-    super(color);
-  }
+    /**
+     * Constructor
+     * @param name Player's name
+     * @param color Player color: one of Constants.BLACK or Constants.WHITE
+     */
+    public MaldGagCri(int color) {
+        super(color);
+    }
 
-  /**
-   *
-   * @param board
-   * @return The player's next move
-   */
-  @Override
-  public Position getNextMove(Board board) {
-    ArrayList<Position> list = this.getLegalMoves(board);
-    if (list.size() > 0) {
-      int idx = (int) (Math.random() * list.size());
-      return list.get(idx);
-    } else {
-      return null;
+    /**
+     *
+     * @param board
+     * @return The player's next move
+     */
+    @Override
+    public Position getNextMove(Board board, int depth, int max_depth, int chosen_score) {
+        ArrayList<Position> list = this.getLegalMoves(board);
+        if (depth = max_depth) {
+            chosen_score = evaluation(board);
+        } else {
+            list = generate_moves(board);
+            if (list == null) {
+                chosen_score = evaluation(board);
+            } else {
+                for (int i = 1; i < list.size(); i++) {
+                    best_score = infinity;
+                    new_board = board;
+                    apply_move(new_board, moves_list[i]);
+                    minimax(new_board, depth+1, max_depth, the_score, the_move);
+                    if (better(the_score, best_score)) {
+                        int best_score = the_score;
+                        Position best_move = the_move;
+                    }
+                }
+                int chosen_score = best_score;
+                Position chosen_move = best_move;
+            }
+        }
     }
-  }
-  
-  /**
-   * Is this a legal move?
-   * @param player Player asking
-   * @param positionToCheck Position of the move being checked
-   * @return True if this space is a legal move
-   */
-  private boolean isLegalMove(Board board, Position positionToCheck) {
-    for (String direction : Directions.getDirections()) {
-      Position directionVector = Directions.getVector(direction);
-      if (step(board, positionToCheck, directionVector, 0)) {
-        return true;
-      }
+
+    /**
+     * Is this a legal move?
+     * @param player Player asking
+     * @param positionToCheck Position of the move being checked
+     * @return True if this space is a legal move
+     */
+    private boolean isLegalMove(Board board, Position positionToCheck) {
+        for (String direction : Directions.getDirections()) {
+            Position directionVector = Directions.getVector(direction);
+            if (step(board, positionToCheck, directionVector, 0)) {
+                return true;
+            }
+        }
+        return false;
     }
-    return false;
-  }
-  
-  /**
-   * Traverses the board in the provided direction. Checks the status of
-   * each space: 
-   * a. If it's the opposing player then we'll move to the next
-   *    space to see if there's a blank space
-   * b. If it's the same player then this direction doesn't represent
-   *    a legal move
-   * c. If it's a blank AND if it's not the adjacent square then this
-   *    direction is a legal move. Otherwise, it's not.
-   * 
-   * @param player  Player making the request
-   * @param position Position being checked
-   * @param direction Direction to move
-   * @param count Number of steps we've made so far
-   * @return True if we find a legal move
-   */
-  private boolean step(Board board, Position position, Position direction, int count) {
-    Position newPosition = position.translate(direction);
-    int color = this.getColor();
-    if (newPosition.isOffBoard()) {
-      return false;
-    } else if (board.getSquare(newPosition).getStatus() == -color) {
-      return this.step(board, newPosition, direction, count+1);
-    } else if (board.getSquare(newPosition).getStatus() == color) {
-      return count > 0;
-    } else {
-      return false;
+
+    /**
+     * Traverses the board in the provided direction. Checks the status of
+     * each space: 
+     * a. If it's the opposing player then we'll move to the next
+     *    space to see if there's a blank space
+     * b. If it's the same player then this direction doesn't represent
+     *    a legal move
+     * c. If it's a blank AND if it's not the adjacent square then this
+     *    direction is a legal move. Otherwise, it's not.
+     * 
+     * @param player  Player making the request
+     * @param position Position being checked
+     * @param direction Direction to move
+     * @param count Number of steps we've made so far
+     * @return True if we find a legal move
+     */
+    private boolean step(Board board, Position position, Position direction, int count) {
+        Position newPosition = position.translate(direction);
+        int color = this.getColor();
+        if (newPosition.isOffBoard()) {
+            return false;
+        } else if (board.getSquare(newPosition).getStatus() == -color) {
+            return this.step(board, newPosition, direction, count+1);
+        } else if (board.getSquare(newPosition).getStatus() == color) {
+            return count > 0;
+        } else {
+            return false;
+        }
     }
-  }
-  
-  /**
-   * Get the legal moves for this player on the board
-   * @param board
-   * @return True if this is a legal move for the player
-   */
-  public ArrayList<Position> getLegalMoves(Board board) {
-    int color = this.getColor();
-    ArrayList list = new ArrayList<>();
-    for (int row = 0; row < Constants.SIZE; row++) {
-      for (int col = 0; col < Constants.SIZE; col++) {
-        if (board.getSquare(this, row, col).getStatus() == Constants.EMPTY) {
-          Position testPosition = new Position(row, col);
-          if (this.isLegalMove(board, testPosition)) {
-            list.add(testPosition);
-          }
-        }        
-      }
+
+    /**
+     * Get the legal moves for this player on the board
+     * @param board
+     * @return True if this is a legal move for the player
+     */
+    public ArrayList<Position> getLegalMoves(Board board) {
+        int color = this.getColor();
+        ArrayList list = new ArrayList<>();
+        for (int row = 0; row < Constants.SIZE; row++) {
+            for (int col = 0; col < Constants.SIZE; col++) {
+                if (board.getSquare(this, row, col).getStatus() == Constants.EMPTY) {
+                    Position testPosition = new Position(row, col);
+                    if (this.isLegalMove(board, testPosition)) {
+                        list.add(testPosition);
+                    }
+                }        
+            }
+        }
+        return list;
     }
-    return list;
-  }
 
 }
